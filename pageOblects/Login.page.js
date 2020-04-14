671abefc3d24effd185a233cbe3c9d641cfa77db
Julia Stepiko <stepikoys@gmail.com>
2020-04-13 22:19:40 -0700

    clear Login page

--- pageOblects/Login.page.js
+++ pageOblects/Login.page.js
@@ -1,50 +1,9 @@
 import BasePage from './Base.page';
-import ElementUtils from "../helpers/ElementUtils";
+//import ElementUtils from "../helpers/ElementUtils";
 
 class LoginPage extends BasePage {
-    }
-    get emailInput() {
-        return this.getElement('//input[@name="email"]');
-    }
 
-    get passwordInput() {
-        return $('//input[@name="password"]');
-    }
 
-    get forgotPasswordLink() {
-        return $('//a[@qa="forgot-password-link"]');
-    }
-
-    get submitBtn() {
-        return $('//button[@type="submit"]');
-    }
-
-    get notification() {
-        return $('//h4[@class="notification-title"]');
-    }
-
-    get invalidEmailNotification(){
-        return $('//div[@class="invalid-feedback"]')
-    }
-
-    login(role) {
-        this.open();
-        this.emailInput.setValue(role.email);
-        this.passwordInput.setValue(role.password);
-        this.submitBtn.click();
-        browser.pause(2000);
-    }
-
-    open(path) {
-        browser.url(`${path}`);
-    }
-
-    expectedElements() {
-        return {
-            this.emailInput,
-            this.
-        }
-    }
 }
 
 export default new LoginPage();
