878c9f8e078d47b6fee6f65bb8aa025ea1fa6dd4
Julia Stepiko <stepikoys@gmail.com>
2020-04-13 23:13:07 -0700

    Login Page finish

--- pageOblects/Login.page.js
+++ pageOblects/Login.page.js
@@ -1,9 +1,36 @@
 import BasePage from './Base.page';
-//import ElementUtils from "../helpers/ElementUtils";
 
 class LoginPage extends BasePage {
 
+    get emailInput() {
+        return this.getElement('input[name="email"]');
+    }
 
+    get passwordInput() {
+        return this.getElement('input[name="password"]');
+    }
+
+    get loginButton() {
+        return this.getElements('.btn')[1];
+    }
+
+    get forgotPasswordLink() {
+        return this.getElement('.btn-');
+    }
+
+    get registerLink() {
+        return this.getElements('.btn')[0];
+    }
+
+    expectedElements() {
+        return {
+            this.emailInput,
+            this.passwordInput,
+            this.loginButton,
+            this.forgotPasswordLink,
+            this.registerLink
+        }
+    }
 }
 
 export default new LoginPage();
